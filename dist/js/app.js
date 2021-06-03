63fd95ce229139396c07327da770e4a8f79e944b
Tomas Rodriguez <tomirodriguez1368@gmail.com>
2021-06-03 20:56:48 -0300

    Subida de archivos

---
+++ dist/js/app.js
@@ -0,0 +1,55 @@
+const menuBtn = document.querySelector(".nav-bar__burger"),
+HamburgerButton = document.querySelector(".nav-bar__burger__span"),
+nav = document.querySelector(".bar"),
+menuNav = document.querySelector(".bar-ok"),
+ocult = document.querySelector(".section-flex");
+
+
+
+
+if (window.innerWidth > 300){
+    window.sr = ScrollReveal();
+        sr.reveal(".projects-container", {
+            duration: 2000,
+            origin:"left",
+            distance:"-100px"
+        });
+        sr.reveal(".move",{
+            duration: 2000,
+            origin:"right",
+            distance:"-100px",
+            reset:"true"
+        })
+        sr.reveal(".move2", {
+            duration: 2000,
+            origin:"left",
+            distance:"-100px",
+            reset:"true"
+        })
+        sr.reveal(".middle", {
+            duration: 2000,
+            origin:"top",
+            distance:"-100px",
+            reset:"true"
+        })
+}
+
+
+
+let showMenu = false;
+
+menuBtn.addEventListener("click",()=>{
+    if (!showMenu) {
+        HamburgerButton.classList.add("open");
+        nav.classList.add("open");
+        menuNav.classList.add("open");
+        ocult.classList.add("ocult");
+        showMenu = true;
+    } else {
+        HamburgerButton.classList.remove("open");
+        nav.classList.remove("open");
+        menuNav.classList.remove("open");
+        ocult.classList.remove("ocult");
+        showMenu = false;
+    }
+})
